b31d2e9d8748e6c1854730149c3cb94d790fc583
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2023-11-12 12:27:08 +0800

    Added docker-container-id to get Docker container id

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const Koa = require("koa");
 const app = new Koa();
 const morgan = require("koa-morgan");
 const router = require("./routes");
+const getDockerId = require("docker-container-id");
 
 // Morgan to log HTTP requests
 app.use(morgan("combined", {}));
@@ -23,6 +24,12 @@ app.use(router.allowedMethods());
 
 // Start the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`ardapi is running on port ${port}`);
+app.listen(port, async () => {
+  console.log(
+    `ardapi is running on port ${port}, ${
+      (await getDockerId()) === false
+        ? "this instance is not containerized."
+        : "Docker ID is " + (await getDockerId())
+    }`
+  );
 });
